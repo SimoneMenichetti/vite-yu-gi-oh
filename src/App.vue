38f05fda92f7a6890e98e825165305ea5e98092c
simone menichetti <simon3menichettui95@gmail.com>
2024-07-09 16:20:36 +0200

    feat: Header(add template and style css , import in App and add message text for main title)

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,22 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script >
+// importiamo il componente AppHeader
+import AppHeader from './components/AppHeader.vue'
+
+export default{
+  name:"App",
+  components:{
+    AppHeader,
+  }
+}
 </script>
 
 <template>
-
-  
+  <!-- identifichiamo con message il testo del titolo -->
+  <AppHeader message="Yu-Gi-Oh Api"/>
 </template>
 
 <style lang="scss">
-@use './assets/style/partials/general.scss';
+@use './assets/style/partials/general.scss' ;
 
 main{
   padding-top:20px
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,35 @@
+<script>
+export default{
+    name:'AppHeader',
+    props:["message"]
+}
+</script>
+
+<template>
+    <!-- inserimento logo e titolo header -->
+<header class="header">
+  <img src="" alt="Logo">
+  <h1 class="mb-0">{{message}}</h1>
+</header>
+
+</template>
+
+<style scoped lang="scss">
+@use'../assets/style/partials/general.scss';
+
+// regole header
+.header {
+      display: flex;
+      align-items: center;
+      padding: 10px;
+      background-color: #f8f9fa; 
+    }
+    .header img {
+      max-height: 50px;
+      margin-right: 20px;
+    }
+    .header h1 {
+      margin-bottom: 0;
+      font-size: 30px; 
+    }
+</style>
