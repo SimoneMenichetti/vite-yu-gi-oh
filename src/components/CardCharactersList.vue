bfdf70de58043091f4cc82c106f72ce3e2f47a02
simone menichetti <simon3menichettui95@gmail.com>
2024-07-09 22:50:15 +0200

    feat:(add founder counter )

--- src/components/CardCharactersList.vue
+++ src/components/CardCharactersList.vue
@@ -19,26 +19,32 @@ export default{
 </script>
 
 <template>
-<div class="container ">
-    <div class="row card-container">
-        <div v-for="character in store.CardCharactersList" :key="character.id" class="col-6 col-md-4 col-lg-3 mb-5">
-            <!-- componenti cards -->
-             <SingleCharacterCard :info="character"/>
+    <div class="container">
+        <div class="counter bg-dark text-white py-2">
+            <h2 class="text-center mt-6">Found {{ store.CardCharactersList.length }} cards</h2>
         </div>
-        
+        <div class="row card-container">
+            <div v-for="character in store.CardCharactersList" :key="character.id" class="col-6 col-md-4 col-lg-3 mb-5">
+            <!-- Componenti cards -->
+            <SingleCharacterCard :info="character"/>
+            </div>
+        </div>
     </div>
-</div>
 </template>
 
 <style scoped lang="scss">
 @use'../assets/style/partials/general.scss' as*;
 
+.container{
+    .card-container {
+        padding-top: 40px;
+        padding-left: 60px;
+        padding-right: 60px;
+        
 
-.card-container {
-    margin-top: 20px;
-
-    .card{
-        background-color: #D48F38;
+        .card{
+            background-color: #D48F38;
+        }
     }
 }
 
--- src/components/SingleCharacterCard.vue
+++ src/components/SingleCharacterCard.vue
@@ -1,20 +1,31 @@
 <script>
+
 // esportiamo il componente SingleCharacterCard
-export default{
-    name:'SingleCharacterCard',
-    props:['info'],
+export default {
+  name: 'SingleCharacterCard',
+  props: ['info'],
+//   utilizzo di computed per ritornare se archetype o meno
+  computed: {
+    hasArchetype() {
+      return !!this.info.archetype;
+    }
+  }
 }
 </script>
 
 <template>
-    <div class="card text-center ">
+    <div class="card text-center pb-5 ">
         <img :src="info.card_images[0].image_url" alt="" class="img-fluid mb-3 ">
-        
         <h5 class="card-title font-weight-bold text-white ">
             {{info.name}}
         </h5>
-        <p class="card-text">
+        <!-- se è archetype itera archetype e msotra -->
+        <p  v-if="hasArchetype"  class="card-text">
             {{info.archetype}}
+        </p>
+        <!-- se non è archetype itera race e mostra -->
+        <p v-else class="card-text">
+            {{info.race}}
         </p>
     </div>
     
@@ -25,11 +36,16 @@ export default{
 @use'../assets/style/partials/general.scss' as*;
 
 .card-title {
-      font-size: 14px;
-    }
+    font-size: 14px;
+    font-weight: 700;
+}
 
-    .card-text {
-      font-size: 10px;
+.card-text {
+    font-size: 12px;
+    font-size: smaller;
      
-    }
+}
+
+
+
 </style>
