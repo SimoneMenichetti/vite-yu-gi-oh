3676e6f951d920d69805bc45cac6c5d967081100
simone menichetti <simon3menichettui95@gmail.com>
2024-07-09 18:17:13 +0200

    build:(add store.js , add apiUrl and create const store reactive, add import store and data store)

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,23 @@
 <script >
 // importiamo il componente AppHeader
-import AppHeader from './components/AppHeader.vue'
+import AppHeader from './components/AppHeader.vue';
 // importiamo il componente CardCharactersList
 import CardCharactersList from './components/CardCharactersList.vue';
+
+// import dello store
+import { store } from '../src/store';
 
 export default{
   name:"App",
   components:{
     AppHeader,
     CardCharactersList,
-  }
+  },
+  data(){
+    return{
+      store,
+    }
+  },
 }
 </script>
 
