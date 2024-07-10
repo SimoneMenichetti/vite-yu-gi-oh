984a366503dfe309e721f7310d07d1e05128bf20
simone menichetti <simon3menichettui95@gmail.com>
2024-07-10 16:53:47 +0200

    feat:Appsearch( import in App and build static structure)

--- src/components/AppSearch.vue
+++ src/components/AppSearch.vue
@@ -6,16 +6,32 @@ export default{
     data(){
         return{
             store,
+            // creata variab per la select di default
+             selectedCategory: '',
         }
     }
 }
 </script>
 
 <template>
- <div class="col-1 ms-5 ps-3 ">
-      <select v-model="selectedCategory" class="form-select">
-        <option value="Alien">Alien</option>
-        <!-- Add other categories here -->
+ <div class="col-2 ms-5 ps-3 ">
+    <select v-model="selectedCategory" class="form-select">
+        <option value="" disabled selected hidden>Select type</option>
+        <option value="">Alien</option>
+        <option value="">Infernoble Arms</option>
+        <option value="">Noble Knight</option>
+        <option value="">Melodious</option>
+        <option value="">Archfiend</option>
+        <option value="">A.I.</option>
+        <option value="">ABC</option>
+        <option value="">Abyss Actor</option>
+        <option value="">Abyss Script</option>
+        <option value="">Abyss-</option>
+        <option value="">Adamancipator</option>
+        <option value="">Advanced Crystal Beast</option>
+        <option value="">Adventurer Token</option>
+        <option value="">Aesir</option>
+        
       </select>
 </div>
 </template>
